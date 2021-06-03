7298f92b97e8111864b39eb35ae1918728f118fc
aliya-najeeb <84628962+aliya-najeeb@users.noreply.github.com>
2021-06-03 21:49:12 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -24,3 +24,16 @@ int main(void) {
   }
 }
 
+float wallis_pi(int n)
+{ float pi=1.0;
+  for (int i=1;i<=n;i++){
+    
+  pi*=(4.0*i*i)/(float)((4.0*i*i)-1);
+  
+  
+    }
+
+    return 2.0*pi;
+
+
+  }
